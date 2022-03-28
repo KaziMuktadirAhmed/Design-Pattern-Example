0c5f3a7c6521695b8cfede044b3d6398bfaf3bbd
Kazi Muktadir Ahmed <55475124+KaziMuktadirAhmed@users.noreply.github.com>
2022-03-28 10:17:11 +0600

    Commit

--- src/PatternAbsurdFactory/Driver.java
+++ src/PatternAbsurdFactory/Driver.java
@@ -1,7 +1,27 @@
 package PatternAbsurdFactory;
 
+import PatternFactory.Shape;
+
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
         System.out.println("okay");
+        run();
+    }
+
+    public static void run () {
+        AbstractFactory RoundedFactory = FactoryProducer.getFactory(true);
+        AbstractFactory PointyFactory = FactoryProducer.getFactory(false);
+
+        Scanner scanInput = new Scanner(System.in);
+        System.out.println("Input shape type: ");
+        String shapeType = scanInput.nextLine();
+
+        Shape pointyShape = PointyFactory.getShape(shapeType);
+        Shape roundedShape = RoundedFactory.getShape(shapeType);
+
+        pointyShape.draw();
+        roundedShape.draw();
     }
 }
